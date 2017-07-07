42fc5cb7e8b5a425fe130a479a9205bc305de72e
Ben Tarnoff <btarnoff@pivotal.io>
2017-07-07 15:28:38 -0700

    update release checker bot

--- release-checker-bot/run.sh
+++ release-checker-bot/run.sh
@@ -4,4 +4,4 @@ set -ex
 
 bundle install --gemfile=docs-utility-scripts-1-11/release-checker-bot/Gemfile
 
-ruby docs-utility-scripts-1-11/release-checker-bot/get-stemcells.rb
+ruby docs-utility-scripts-1-11/release-checker-bot/get-releases.rb
